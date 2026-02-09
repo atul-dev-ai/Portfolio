cf16973a61fa7356521ae5eb37682600a53528cd
Atul Paul <182104349+Atul-codee@users.noreply.github.com>
2026-02-10 03:44:31 +0600

    Vercel Analytics and Vercel SpeedInsights install for speed and traffic tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Navbar from "@/components/ui/navbar";
 import Footer from "@/components/ui/Footer";
@@ -81,8 +83,11 @@ export default function RootLayout({
         >
           <Navbar />
           {children}
+
           <Toaster />
           <Footer />
+          <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
